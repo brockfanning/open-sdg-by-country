66808aeb2ba4177431f3147e6e0ffc978d0ea534
Brock Fanning <brockfanning@gmail.com>
2021-01-21 18:29:57 -0500

    Disagg cleanup

--- build.py
+++ build.py
@@ -21,8 +21,9 @@ def get_site_config(ref_area_id, ref_area_name):
     with open('config_site.yml', 'r') as stream:
         config = yaml.load(stream, Loader=yaml.FullLoader)
     config['title'] = ref_area_name + ' Indicators for the Sustainable Development Goals'
-    config['baseurl'] = '/open-sdg-by-country/' + ref_area_id
-    config['remote_data_prefix'] = '../../data/' + ref_area_id
+    config['baseurl'] = '/' + ref_area_id
+    config['remote_data_prefix'] = '../data/' + ref_area_id
+    config['destination'] = './' + ref_area_id
     config['country'] = {
         'name': ref_area_name,
         'adjective': ref_area_name
@@ -32,7 +33,7 @@ def get_site_config(ref_area_id, ref_area_name):
 
 def build_site(ref_area_id, ref_area_name):
 
-    site_folder = os.path.join('_builds', 'site', ref_area_id)
+    site_folder = os.path.join('_builds', 'site')
     data_folder = os.path.join('_builds', 'data', ref_area_id)
     Path(site_folder).mkdir(parents=True, exist_ok=True)
     Path(data_folder).mkdir(parents=True, exist_ok=True)
@@ -45,6 +46,17 @@ def build_site(ref_area_id, ref_area_name):
         meta['national_geographical_coverage'] = ref_area_name
         return meta
 
+    drop_dimensions = [
+        'UPPER_BOUND',
+        'LOWER_BOUND',
+        'UNIT_MULT',
+        'COMMENT_OBS',
+        'SOURCE_DETAIL',
+        'BASE_PER',
+        'NATURE',
+        'DATA_LAST_UPDATE',
+        'TIME_DETAIL',
+    ]
     sdg.open_sdg_build(
         site_dir=data_folder,
         schema_file='_prose.yml',
@@ -54,7 +66,13 @@ def build_site(ref_area_id, ref_area_name):
             {'class': 'TranslationInputSdmx', 'source': get_dsd_url()},
         ],
         inputs=[
-            {'class': 'InputSdmxMl_UnitedNationsApi', 'reference_area': ref_area_id},
+            {
+                'class': 'InputSdmxMl_UnitedNationsApi',
+                'reference_area': ref_area_id,
+                'import_codes': True,
+                'drop_singleton_dimensions': True,
+                'drop_dimensions': drop_dimensions,
+            },
         ],
         alter_meta=alter_meta,
         docs_subfolder='data-docs',
